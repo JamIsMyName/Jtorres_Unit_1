7b039586805b87feb2386ee676fe05307f273c07
Jacob <66225072+JamIsMyName@users.noreply.github.com>
2021-02-14 22:28:28 -0800

    added movement and turning

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 25;
+    //forward backwards movement
+    private float speed = 25f;
+    private float turnSpeed = 25f;
+
+
+    private float horizontalInput;
+    private float verticalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,10 +20,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
         Debug.Log(Time.deltaTime);
         // Vector3. forward --> (0, 0, 1)
         // (0, 0, 1) * Time.deltatime = (0, 0, .001)
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
-
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
+        transform.Translate(Vector3.right * turnSpeed * Time.deltaTime * horizontalInput);
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
     }
 }
